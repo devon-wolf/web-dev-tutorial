69ab8ddfcc44e6f7a32bdcad8e87bd3afd315332
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-09 19:35:21 -0700

    Initial DOM updates

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const button = document.getElementById('box-button');
 
 console.log(button);
 
+let count = 0;
+
 button.addEventListener('click', () => {
 	console.log('you clicked!');
-})
+	count++;
+	button.textContent = count;
+});
